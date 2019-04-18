502f52e9eb4f08f59b4a1b8c69b2d1d683df14e2
Prageeth Jayathissa <p.jayathissa@gmail.com>
2019-04-19 02:56:12 +0800

    removed login but text input doesnt work yet

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,23 +1,6 @@
 function mySettings(props) {
   return (
     <Page>
-      <Section
-        title={<Text bold align="center">Fitbit Account</Text>}>
-        <Oauth
-          settingsKey="oauth"
-          title="Fitbit Login"
-          label="Fitbit"
-          status="Login"
-          authorizeUrl="https://www.fitbit.com/oauth2/authorize"
-          requestTokenUrl="https://api.fitbit.com/oauth2/token"
-          clientId="XXXXX"
-          clientSecret="XXXXX"
-          scope="profile"
-          onAccessToken={async (data) => {
-            props.settingsStorage.setItem('user_id', data['user_id'])
-          }}
-        />
-      </Section>
 
       <Section>
       <TextInput
