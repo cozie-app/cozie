ea95de33284481c248277af3b4f99690b1233843
Talantbekov Kairat <talantbekov123@gmail.com>
2019-04-25 13:06:12 +0600

    No code change, just formated tabs/spaces for settigs file

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,52 +1,70 @@
 function mySettings(props) {
   return (
     <Page>
+      <Section>
+        <TextInput
+          label="User ID"
+          settingsKey="user_id"
+          oninput={selection =>
+            props.settingsStorage.setItem("user_id", selection)
+          }
+        />
+      </Section>
 
       <Section>
-      <TextInput
-    label="User ID"
-    settingsKey="user_id"
-    oninput={(selection) => props.settingsStorage.setItem('user_id', selection)}
-  />
-
+        <TextInput
+          label="Unique Experiment ID"
+          settingsKey="experiment_id"
+          oninput={selection =>
+            props.settingsStorage.setItem("experiment_id", selection)
+          }
+        />
       </Section>
-
-            <Section>
-      <TextInput
-    label="Unique Experiment ID"
-    settingsKey="experiment_id"
-    oninput={(selection) => props.settingsStorage.setItem('experiment_id', selection)}
-  />
-
-      </Section>
-
 
       <Section>
         <Select
-  label={`Select Questions`}
-  multiple
-  settingsKey="flow_index"
-  options={[
-    {name:"Thermal",   value: "showThermal", subLabel: "Prefer Warmer, Prefer Cooler, Comfy"},
-    {name:"Light",   value:"showLight", subLabel: "Prefer Brighter, Prefer Dimmer, Comfy"},
-    {name:"Noise", value:"showNoise", subLabel: "Prefer Louder, Prefer Quieter, Comfy"},
-    {name:"Indoor / Outdoor", value:"showIndoor", subLabel: "Indoor, Outdoor"},
-    {name:"In Office", value:"showInOffice", subLabel: "In Office, Out of Office"},
-    {name:"Mood", value:"showMood", subLabel: "Good, Not So Good"},
-  ]}
-  renderItem={
-    (option) =>
-      <TextImageRow
-        label={option.name}
-        sublabel={option.subLabel}
-        icon="https://tinyurl.com/ybbmpxxq"
-      />
-  }
-//  onSelection={(selection) => { 
-//    props.settingsStorage.setItem('flow_index', JSON.stringify(selection))
-//  }} 
-/>
-
+          label={`Select Questions`}
+          multiple
+          settingsKey="flow_index"
+          options={[
+            {
+              name: "Thermal",
+              value: "showThermal",
+              subLabel: "Prefer Warmer, Prefer Cooler, Comfy"
+            },
+            {
+              name: "Light",
+              value: "showLight",
+              subLabel: "Prefer Brighter, Prefer Dimmer, Comfy"
+            },
+            {
+              name: "Noise",
+              value: "showNoise",
+              subLabel: "Prefer Louder, Prefer Quieter, Comfy"
+            },
+            {
+              name: "Indoor / Outdoor",
+              value: "showIndoor",
+              subLabel: "Indoor, Outdoor"
+            },
+            {
+              name: "In Office",
+              value: "showInOffice",
+              subLabel: "In Office, Out of Office"
+            },
+            { name: "Mood", value: "showMood", subLabel: "Good, Not So Good" }
+          ]}
+          renderItem={option => (
+            <TextImageRow
+              label={option.name}
+              sublabel={option.subLabel}
+              icon="https://tinyurl.com/ybbmpxxq"
+            />
+          )}
+          //  onSelection={(selection) => {
+          //    props.settingsStorage.setItem('flow_index', JSON.stringify(selection))
+          //  }}
+        />
       </Section>
     </Page>
   );
