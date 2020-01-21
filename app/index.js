f1bdabf95a2a55454724dd27f7295e465ceda58a
FedericoTartarini <federicotartarini@gmail.com>
2020-01-21 09:09:09 +0800

    extended buzzer timer 21:00 and changed description

--- app/index.js
+++ app/index.js
@@ -74,9 +74,9 @@ let local_file;
 // function that runs in the background and start vibration to remind the user to complete the survey
 const buzzOptions = {
     '0': [],
-    '1': [9, 10, 11, 12, 13, 14, 15, 16, 17],
-    '2': [9, 11, 13, 15, 17],
-    '3': [9, 12, 15]
+    '1': [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+    '2': [9, 11, 13, 15, 17, 19, 21],
+    '3': [9, 12, 15, 18, 21]
 };
 
 const bodyPresence = new BodyPresenceSensor();
